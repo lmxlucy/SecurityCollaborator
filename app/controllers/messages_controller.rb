42687d2a8ba3e14c7d86bf8e3445cf230b4704eb
Lucy <meixuanl@andrew.cmu.edu>
2021-01-13 22:24:30 -0500

    small changes to messages_controller params

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -71,7 +71,7 @@ class MessagesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def message_params
-      params.require(:message).permit(:user_id, :alerts, :reminders, 
-      :device_alerts, :device_reminders)
+      params.require(:message).permit(:user_id, :date, {:alerts=>[]}, {:reminders=>[]}, 
+      {:device_alerts=>[]}, {:device_reminders=>[]}, :perfect)
     end
 end
