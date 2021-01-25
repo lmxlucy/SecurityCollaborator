2ea971d89c0e6072658ce1b49e50dd82d11e9b93
Lucy <meixuanl@andrew.cmu.edu>
2021-01-25 17:42:02 -0500

    couldn't fix email problem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,7 +117,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => "gmail.com",
     :user_name            => ENV['gmail_username'],
     :password             => ENV['gmail_password'],
     :authentication       => "plain",
