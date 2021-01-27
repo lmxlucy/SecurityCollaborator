cf97f49e8c729d1c1a83e78f7b10c0753ba884bf
Lucy <meixuanl@andrew.cmu.edu>
2021-01-26 19:13:22 -0500

    figaro working with heroku now

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,8 +67,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => "asharing.scollaborator@gmail.com",
-    :password             => "Securitycollaborator123",
+    :user_name            => ENV['gmail_username'],
+    :password             => ENV['gmail_password'],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
