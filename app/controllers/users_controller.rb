dbc020ce9607ac7ffc3f7f278e0c8122bd544345
Lucy <meixuanl@andrew.cmu.edu>
2021-04-12 22:15:03 -0400

    change set_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,6 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user=current_user
   end
 
   # GET /users/new
@@ -342,11 +341,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      if @current_user
-        @user = @current_user
-      else
-        @user = User.find(params[:id])
-      end
+      @user = User.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
